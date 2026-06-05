5f8fa6a95ceeb38fbc19651107e3c9cd0e28f5ad
ig-aditya-101 <adistuding@gmail.com>
2026-06-05 12:30:43 +0530

    init commit

---
+++ Week-06/medium/weather-switcher/my-react-app/src/pages/weatherCard.jsx
@@ -0,0 +1,12 @@
+export function Weather() {
+  //call api when user select the city
+  //optiion to select city
+  //initial default city cghangeable
+  //ui
+  // button select city
+  // button to change the ui theme
+  // theme selector bunch of differemnt colors
+  // select color => api calll
+  //
+  return <></>;
+}
